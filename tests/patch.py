207a157e3d6bf84a06bd1a86711291cd21516626
Jon Betts <jon.betts@hypothes.is>
2019-10-21 11:50:16 +0100

    Further splitting up the template and module

---
+++ tests/patch.py
@@ -0,0 +1,19 @@
+import functools
+import pytest
+
+from unittest import mock
+
+
+def _autopatcher(request, target, **kwargs):
+    """Patch and cleanup automatically. Wraps :py:func:`mock.patch`."""
+    options = {"autospec": True}
+    options.update(kwargs)
+    patcher = mock.patch(target, **options)
+    obj = patcher.start()
+    request.addfinalizer(patcher.stop)
+    return obj
+
+
+@pytest.fixture
+def patch(request):
+    return functools.partial(_autopatcher, request)
--- {{ cookiecutter.project_slug }}/tests/integration/conftest.py
+++ {{ cookiecutter.project_slug }}/tests/integration/conftest.py
@@ -1,6 +1,1 @@
-"""
-The `conftest` module is automatically loaded by pytest and serves as a place
-to put fixture functions that are useful application-wide.
-"""
-
-from tests.patch import patch
+../../../tests/integration/conftest.py
