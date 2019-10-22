06cd863d2f3769077e823fe16495f56afd40eb86
Jon Betts <jon.betts@hypothes.is>
2019-10-22 13:00:26 +0100

    Formatting issues

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,4 +3,4 @@ The `conftest` module is automatically loaded by pytest and serves as a place
 to put fixture functions that are useful application-wide.
 """
 
-from tests.patch import patch   # pylint: disable=unused-import
+from tests.patch import patch  # pylint: disable=unused-import
