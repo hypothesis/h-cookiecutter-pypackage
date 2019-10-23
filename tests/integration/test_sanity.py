61cfaa77982747494c210181e5737362dbbd6a22
Jon Betts <jon.betts@hypothes.is>
2019-10-23 13:12:41 +0100

    Adding basic checks that we have similar files in the root and template
    This is to catch cases where we add a file required to continue, and
    we don't remember to put it in the template.

---
+++ tests/integration/test_sanity.py
@@ -0,0 +1,42 @@
+import os
+
+
+class TestSanity:
+    """
+    Some basic 'the world isn't falling down' tests. These do not ensure
+    anything is fine, but if any of these break you definitely have trouble.
+    """
+
+    def test_all_files_in_root_accounted_for(self, template):
+        template_dir = "{{ cookiecutter.project_slug }}"
+        expected_files = set(os.listdir(template))
+        templated_files = set(os.listdir(os.path.join(template, template_dir)))
+
+        missing = expected_files - templated_files
+
+        # Remove a bunch of files we expect to see around, but do not come
+        # from the template
+        missing -= {
+            # OS specific noise
+            ".DS_store",
+            # General development waffle
+            ".git",
+            ".idea",
+            ".tox",
+            ".pytest_cache",
+            ".eggs",
+            "dist",
+            "__pycache__",
+            "h_cookiecutter_pypackage.egg-info",
+            # Expected resources that shouldn't be there
+            ".cookiecutter.json",
+            "cookiecutter.json",
+            "hooks",
+            "h_cookiecutter_pypackage",
+            template_dir,
+        }
+
+        # Coverage produces files with un-predictable names. Strip them out
+        missing = {name for name in missing if not name.startswith(".coverage.")}
+
+        assert not missing
