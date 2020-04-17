15170f76ce9828d33be75e1fbebeeecf32e4edce
Jon Betts <jon.betts@hypothes.is>
2020-04-17 13:40:13 +0100

    Fix two bugs with deployment and documentation
    * Fix the link to source which previously pointed to the cookie cutter
    * Fix cookiecutter changing to use an OrderedDict instead of a plain one
    
    This resulted in us templating out an invalid script as we didn't
    load OrderedDict.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,8 @@
 import json
+# Looks like cookiecutter has started using an OrderedDict for the
+# `cookiecutter` object. If we don't import this first we will fail when
+# that is included in our script.
+from collections import OrderedDict
 
 # Save the cookie cutter config to replay later
 with open(".cookiecutter.json", "w") as fh:
